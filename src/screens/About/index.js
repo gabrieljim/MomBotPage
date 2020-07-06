4b4cc5cd5cdd4cb9afd498a47a058c0f9261ecb2
gabrieljim <gabrieljim641@gmail.com>
2020-07-06 17:48:22 -0400

    removed dead code

--- src/screens/About/index.js
+++ src/screens/About/index.js
@@ -3,28 +3,30 @@ import "./styles.css";
 import * as SC from "./styles";
 
 function importAll(r) {
-  return r.keys().map(r);
+	return r.keys().map(r);
 }
 
+/*
 const images = importAll(
   require.context("../../assets", false, /\.(png|jpe?g|svg)$/)
 );
+*/
 
 const About = () => {
-  return (
-    <SC.About>
-      <SC.Info>
-        <h1>
-          What is <SC.MomTitle>Mom</SC.MomTitle> Bot for?
-        </h1>
-        <p>Mom was made in memory of a very special cat.</p>{" "}
-        <p>
-          I wanted to commemorate my love for mom the cat in the form of this, a
-          discord bot that all can enjoy.
-        </p>
-      </SC.Info>
-    </SC.About>
-  );
+	return (
+		<SC.About>
+			<SC.Info>
+				<h1>
+					What is <SC.MomTitle>Mom</SC.MomTitle> Bot for?
+				</h1>
+				<p>Mom was made in memory of a very special cat.</p>{" "}
+				<p>
+					I wanted to commemorate my love for mom the cat in the form of this, a
+					discord bot that all can enjoy.
+				</p>
+			</SC.Info>
+		</SC.About>
+	);
 };
 
 export default About;
